90f85a0fe0e9d0573f922b5dfbaadf9d1a292ef8
adelyanasretdinova <adelya.nasretdinova@gmail.com>
2022-10-26 18:32:43 +0200

    filter country cuisine works

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -1,110 +1,39 @@
+import Dropdown from "react-bootstrap/Dropdown";
+
 const FilterButton = (props) => {
   return (
-    <div
-      className="filterButtons d-flex flex-row justify-content-around"
-      style={{ width: "90rem" }}
-    >
-      <div className="dropdown">
-        <button
-          className="btn btn-secondary dropdown-toggle"
-          type="button"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
+    <Dropdown>
+      <Dropdown.Toggle variant="success" id="dropdown-basic">
+        Country cuisine
+      </Dropdown.Toggle>
+      <Dropdown.Menu>
+        {props.countriesCuisine.map((element) => (
+          <Dropdown.Item
+            className="dropdown-item"
+            onClick={props.displayCountryCuisine}
+            id={element}
+            key={element}
+          >
+            {element}
+          </Dropdown.Item>
+        ))}
+        <Dropdown.Item
+          className="dropdown-item"
+          onClick={props.resetCountryCuisine}
+          id="resetEvent"
         >
-          Country cuisine
-        </button>
-        <ul className="dropdown-menu">
-          <li>
-            {props.countriesCuisine.map((element) => (
-              <button
-                className="dropdown-item"
-                onClick={props.displayCountryCuisine}
-                id={element}
-                key={element}
-              >
-                {element}
-              </button>
-            ))}
-          </li>
+          All
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
 
-          <li>
-            <button
-              className="dropdown-item"
-              onClick={props.resetCountryCuisine}
-              id="resetEvent"
-            >
-              All
-            </button>
-          </li>
-        </ul>
-      </div>
-      {/* <div className="dropdown">
-        <button
-          className="btn btn-secondary dropdown-toggle"
-          type="button"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          Meal type
-        </button>
-        <ul className="dropdown-menu">
-          <li>
-            <button
-              className="dropdown-item"
-              onClick={props.filter}
-              id="Dessert"
-            >
-              Dessert
-            </button>
-          </li>
-          <li>
-            <button
-              className="dropdown-item"
-              onClick={props.filter}
-              id="SideDish"
-            >
-              Side dish
-            </button>
-          </li>
-          <li>
-            <button
-              className="dropdown-item"
-              onClick={props.filter}
-              id="Vegetarian"
-            >
-              Vegetarian
-            </button>
-          </li>
-          <li>
-            <button
-              className="dropdown-item"
-              onClick={props.filter}
-              id="Breakfast"
-            >
-              Breakfast
-            </button>
-          </li>
-          <li>
-            <button
-              className="dropdown-item"
-              onClick={props.filter}
-              id="DinnerParty"
-            >
-              Dinner Party
-            </button>
-          </li>
-          <li>
-            <button
-              className="dropdown-item"
-              onClick={props.reset}
-              id="resetEvent"
-            >
-              All
-            </button>
-          </li>
-        </ul>
-      </div> */}
-    </div>
+    //       <li>
+    //         <button
+
+    //         >
+    //           All
+    //         </button>
+    //
   );
 };
 export default FilterButton;
--- src/components/RecipesCards.js
+++ src/components/RecipesCards.js
@@ -37,56 +37,54 @@ const RecipesCard = (props) => {
   console.log("combinedIngredients", combinedIngredients);
 
   return (
-    <>
-      <Card className="card m-2" style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={mealPic} />
-        <Card.Body>
-          <Card.Title>{mealName}</Card.Title>
-          <Card.Text>
-            <p className="tags fst-italic">
-              {mealTag}
-              {mealOrigin}
-            </p>
-          </Card.Text>
-          <Button
-            type="button"
-            className="btn btn-danger position-absolute top-0 end-0 opacity-85"
-          >
-            <i className="bi bi-bookmark-heart-fill"></i> Save to favorites
-          </Button>
-          <Button
-            onClick={() => setOpen(!open)}
-            aria-controls="example-collapse-text"
-            aria-expanded={open}
-          >
-            See More
-          </Button>
-          <Collapse in={open}>
-            <div id="example-collapse-text">
-              <div>
-                <h5>Ingredients:</h5>
-              </div>
-              <div>
-                <ul className="ingredients">
-                  {combinedIngredients.map(function (item) {
-                    return (
-                      <li key={item}>
-                        {item[0]}: {item[1]}
-                      </li>
-                    );
-                  })}
-                </ul>
-              </div>
-
-              <div>
-                <h5>Preparations:</h5>
-                <p>{instructions}</p>
-              </div>
+    <Card className="card m-2" style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={mealPic} />
+      <Card.Body>
+        <Card.Title>{mealName}</Card.Title>
+        <Card.Text>
+          <p className="tags fst-italic">
+            {mealTag} <br />
+            {mealOrigin}
+          </p>
+        </Card.Text>
+        <Button
+          type="button"
+          className="btn btn-danger position-absolute top-0 end-0 opacity-85"
+        >
+          <i className="bi bi-bookmark-heart-fill"></i> Save to favorites
+        </Button>
+        <Button
+          onClick={() => setOpen(!open)}
+          aria-controls="example-collapse-text"
+          aria-expanded={open}
+        >
+          See More
+        </Button>
+        <Collapse in={open}>
+          <div id="example-collapse-text">
+            <div>
+              <h5>Ingredients:</h5>
             </div>
-          </Collapse>
-        </Card.Body>
-      </Card>
-    </>
+            <div>
+              <ul className="ingredients">
+                {combinedIngredients.map(function (item) {
+                  return (
+                    <li key={item}>
+                      {item[0]}: {item[1]}
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+
+            <div>
+              <h5>Preparations:</h5>
+              <p>{instructions}</p>
+            </div>
+          </div>
+        </Collapse>
+      </Card.Body>
+    </Card>
   );
 };
 
