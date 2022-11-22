0716f50e910968e9095fb26260c81714d5a5f7a8
adelyanasretdinova <adelya.nasretdinova@gmail.com>
2022-11-22 17:35:57 +0100

    added UI for searchbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -50,6 +50,21 @@ const Header = () => {
               About us
             </Link>
           </li>
+          <nav class="navbar bg-light">
+            <div class="container-fluid">
+              <form class="d-flex" role="search">
+                <input
+                  class="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <button class="btn btn-outline-success" type="submit">
+                  Search
+                </button>
+              </form>
+            </div>
+          </nav>
         </ul>
       </nav>
     </header>
