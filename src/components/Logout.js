ab9bf8d37bc6357d48f618d1510518773b68c66c
adelyanasretdinova <adelya.nasretdinova@gmail.com>
2022-11-24 20:27:49 +0100

    bug fixed: styled logout buttons

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -4,6 +4,8 @@ const Logout = (props) => {
       <h2>Hope to see you back soon!</h2>
       <p>Click here to log out:</p>
       <button
+        type="submit"
+        className="btn btn-primary"
         onClick={() => {
           localStorage.removeItem("token");
           props.setLoggedIn(false);
